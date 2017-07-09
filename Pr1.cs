74367b080650e55cf983c134b59466007ba9e84c
Dmitry Alexandrov <rus22imp@mail.ru>
2017-07-09 20:25:28 +0500

    Program 1.1

---
+++ Pr1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR1
+{
+    class Pr1
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, p;
+            Console.Write("Введите длину параллелепипеда: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите ширину параллелепипеда: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите высоту параллелепипеда: ");
+            c = Convert.ToDouble(Console.ReadLine());
+            p = 2 * (a + b + c);
+            Console.WriteLine("Площадь параллелепипеда = {0}", p);
+            Console.ReadKey();
+
+        }
+    }
+}
